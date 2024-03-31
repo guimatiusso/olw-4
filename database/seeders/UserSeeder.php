cf2c8bfb4445263e15ae2ea9b0c6d9e3b82f97bb
guimatiusso <rottamatiusso@gmail.com>
2024-03-31 20:27:45 -0300

    fix: fix UserSeeder for roles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Seller;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -16,10 +17,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()
-            ->count(100)
-            ->has(Seller::factory()
-                ->hasSales(30))
-            ->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'role_id' => 1,
+            'remember_token' => Str::random(10)
+        ]);
     }
 }
