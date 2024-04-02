e592b15c07b857c064e3d04e267118bab04f2cdf
guimatiusso <rottamatiusso@gmail.com>
2024-04-01 23:02:16 -0300

    feat: WIP first version of sellers crud

---
+++ resources/views/livewire/seller/index.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="px-4 sm:px-6 lg:px-8">
+
+                <x-heading
+                    title="Sellers"
+                    description="A list of all the sellers."
+                    btn-label="Add sellers"
+                    :route="route('sellers.create')" />
+
+                <div class="w-full overflow-hidden md:rounded-lg">
+                    <livewire:table
+                        resource="Seller"
+                        :columns="[
+                            ['label' => 'Seller', 'column' => 'user.name'],
+                            ['label' => 'Company', 'column' => 'company.name'],
+                            ['label' => 'Email','column' => 'user.email'],
+                        ]"
+                        :eager-loading="['user', 'company']"
+                        edit="sellers.edit"
+                        delete="sellers.destroy"
+                    ></livewire:table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
